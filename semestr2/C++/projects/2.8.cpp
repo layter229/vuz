efa509c89ecf7620cdaef3162c6b6774a2cfbd68
layter229 <36046200+layter229@users.noreply.github.com>
2022-03-17 13:46:48 +0500

    Update 2.8.cpp

---
+++ semestr2/C++/projects/2.8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+
+int a,b,c,x,y,z;
+
+int main()
+{   
+    cout << "Vvedite chisla a, b cherez probel"<<endl;
+    cin >>a>>b;
+    if ((a>b) ) {
+        cout<<a;
+    }
+    else {
+         cout<<a<<endl<<b<<endl;
+    }
+    cout <<""<<endl<<"Dlya zaversheniya najmite <ENTER>";
+    getch();
+}
