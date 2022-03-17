00909850d80db349681e2ecc81106b211fed475d
layter229 <36046200+layter229@users.noreply.github.com>
2022-03-17 14:27:58 +0500

    Create 3.3.cpp

---
+++ semestr2/C++/projects/3.3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <cmath>
+using namespace std;
+
+int a,b,c,m,n,z,s;
+
+int main()
+{   
+    cout<<"Vvedite chisla a, b cherez probel"<<endl;
+    cin>>m>>n;
+    for (int i=m;i<n;i+2){
+    s=s+(i*i);
+    }
+    cout<<s<<""<<endl<<"Dlya zaversheniya najmite <ENTER>";
+    getch();
+}
